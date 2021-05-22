1fefcc605a23a6ed17664c394521baf39b59fd8c
vijay Chapagain <spunkytykoon1@gmail.com>
2021-05-22 07:00:19 +0545

    latest commit

---
+++ js/typed.js
@@ -0,0 +1,14 @@
+
+    let animate= new Typed('#ani',{
+        strings:["Front End Developer","youtuber","Blogger"],
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+    })
+
+    let animat= new Typed('#ani1',{
+        strings:["Front End Developer","youtuber","Blogger"],
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+    })
